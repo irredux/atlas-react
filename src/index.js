0881580ac55988a32b8d462dbe8a07bcd77d927c
Alexander Häberlin <alexander.haeberlin@mlw.badw.de>
2022-06-18 18:26:35 +0200

    "update"

--- src/index.js
+++ src/index.js
@@ -26,6 +26,8 @@ function App(){
         case "dom":
             windowTitle = "dDOM";
             break;
+        css "tll":
+            windowTitle = "dTLL";
         default:
             windowTitle = "???";
     }
