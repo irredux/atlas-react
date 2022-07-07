789b8541884e5b34fdd8c401ed6d96d2c8693ce4
Alexander Häberlin <alexander.haeberlin@mlw.badw.de>
2022-07-07 22:33:35 +0200

    u

--- src/editor/zettel.js
+++ src/editor/zettel.js
@@ -264,6 +264,7 @@ function RessourcesButtons(props){
     const [button, setButton] = useState(null);
     const [selected, setSelected] = useState(null);
     const openRessource = (url) => {
+        console.log(url);
         switch(arachne.options.openExternally){
             case 0:
                 props.setRessourceView(url);
@@ -280,7 +281,7 @@ function RessourcesButtons(props){
     }
     useEffect(()=>{
         if(props.ressources.length===0){setButton(null)}
-        else if(props.ressources.length===1){setButton(<Button tabIndex="-1" size="sm" variant="outline-dark" onClick={()=>{openRessource(props.ressources[0].url===null||props.ressources[0].url===""?`/${arachne.project_name}/argos/${props.ressources[0].id}`:props.ressources[0].url)}}>{props.ressources[0].label}</Button>)}
+        else if(props.ressources.length===1){setButton(<Button tabIndex="-1" size="sm" variant="outline-dark" onClick={()=>{openRessource(props.ressources[0].url===null||props.ressources[0].url===""?`/static/index.html?project=${arachne.project_name}&app=argos&site=edition&id=${props.ressources[0].id}`:props.ressources[0].url)}}>{props.ressources[0].label}</Button>)}
         else{
             setSelected(props.ressources[0]);
         }
@@ -288,7 +289,7 @@ function RessourcesButtons(props){
     useEffect(()=>{
         if(selected!=null){
             setButton(<Dropdown as={ButtonGroup}>
-                <Button size="sm" variant="outline-dark" style={{width: "100%"}} onClick={()=>{openRessource(selected.url===null||selected.url===""?`/${arachne.project_name}/argos/${selected.id}`:selected.url)}}>{selected.label}</Button>
+                <Button size="sm" variant="outline-dark" style={{width: "100%"}} onClick={()=>{openRessource(selected.url===null||selected.url===""?`/static/index.html?project=${arachne.project_name}&app=argos&site=edition&id=${selected.id}`:selected.url)}}>{selected.label}</Button>
                 <Dropdown.Toggle split size="sm" variant="dark" id="dropdown-split-basic" />
                 <Dropdown.Menu>
                     {props.ressources.map(r=><Dropdown.Item key={r.id} onClick={()=>{setSelected(r)}}>{r.label}</Dropdown.Item>)}
